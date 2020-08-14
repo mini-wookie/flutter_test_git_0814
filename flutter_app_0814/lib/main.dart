468551d4a2a2cad4f9d9315b2f7e73cff8dc57e8
mini-wookie <54762087+mini-wookie@users.noreply.github.com>
2020-08-14 18:42:27 +0900

    Update main.dart

--- flutter_app_0814/lib/main.dart
+++ flutter_app_0814/lib/main.dart
@@ -56,8 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             buildButtons(),
             buildButtons(),
-            buildButtons(),
-            buildButtons(),
           ],
         ),
       ),
